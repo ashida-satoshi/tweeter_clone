31db43eab9ac7128a43443027d8262fb1b638d0c
ashidasatoshi <dasatoshi2017@yahoo.co.jp>
2019-08-14 23:12:07 +0900

    リダイレクト設定

--- app/controllers/twets_controller.rb
+++ app/controllers/twets_controller.rb
@@ -3,5 +3,12 @@ class TwetsController < ApplicationController
   end
 
   def new
+    @twet = Twet.new
+  end
+
+  def create
+    Twet.create(content: params[:twet][:content])
+
+    redirect_to new_twet_path
   end
 end
