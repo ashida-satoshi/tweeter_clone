358265c7f63ca114fae41a7efe404eed7dac8404
ashidasatoshi <dasatoshi2017@yahoo.co.jp>
2019-08-26 16:26:01 +0900

    コントローラー内コメント削除、css修正

--- app/controllers/twets_controller.rb
+++ app/controllers/twets_controller.rb
@@ -22,15 +22,12 @@ class TwetsController < ApplicationController
   end
 
   def show
-    #@twet =Twet.find(params[:id])
   end
 
   def edit
-    #@twet =Twet.find(params[:id])
   end
 
   def update
-    #@twet = Twet.find(params[:id])
     if @twet.update(twet_params)
       redirect_to twets_path, notice: "編集しました！"
     else
